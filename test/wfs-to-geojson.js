ef43146525c3174f08d8e8f3cef6566ca550d8ef
Cyril CHAPON <cyril@permettezmoideconstruire.fr>
2018-06-29 17:24:02 +0200

    Add more tests, and falling test clockwise

--- test/wfs-to-geojson.js
+++ test/wfs-to-geojson.js
@@ -6,7 +6,8 @@ import {
 } from '../dist/gml-to-geojson'
 import fs from 'fs-extra'
 import path from 'path'
-import { isFeatureCollection, isFeature, positionsOf } from 'gjtk'
+import { isFeatureCollection, isFeature, positionsOf, geometriesOf } from 'gjtk'
+import isClockWise from '@turf/boolean-clockwise'
 
 const {
   featureCollectionToGeoJSON: wfsFeatureCollectionToGeoJSON
@@ -15,12 +16,16 @@ const {
 describe('wfsToGeoJSON function', () => {
   const sampleFileNames = [
     'sample-ok-3.xml',
-    'sample-ko.xml'
+    'sample-ko.xml',
+    'sample-ok-1-cw.xml',
+    'sample-ok-1-ccw.xml'
   ]
 
   const [
     workingSample3FeatFileName,
-    notWorkingSampleEmptyFileName
+    notWorkingSampleEmptyFileName,
+    workingSampleCwFileName,
+    workingSampleCcwFileName
   ] = sampleFileNames
 
   it('should be defined', () => {
@@ -46,6 +51,47 @@ describe('wfsToGeoJSON function', () => {
     })
   })
 
+  describe(`Working samples`, () => {
+    const workingSamplesFileNames = [
+      workingSample3FeatFileName,
+      workingSampleCwFileName,
+      workingSampleCcwFileName
+    ]
+
+    workingSamplesFileNames.forEach(fileName => {
+      it(`should have features with ${fileName}`, () => {
+        const sample = fs.readFileSync(path.resolve(__dirname, fileName), 'UTF-8')
+
+        const parsedFeatureCollection = wfsFeatureCollectionToGeoJSON(sample)
+
+        assert.notNestedPropertyVal(parsedFeatureCollection, 'features.length', 0)
+      })
+
+      it(`should have features with only EPSG:4326 coordinates with ${fileName}`, () => {
+        const sample = fs.readFileSync(path.resolve(__dirname, fileName), 'UTF-8')
+
+        const parsedFeatureCollection = wfsFeatureCollectionToGeoJSON(sample)
+
+        positionsOf(parsedFeatureCollection).forEach(([lng, lat]) => {
+          assert.isBelow(lng, 180)
+          assert.isAbove(lng, -180)
+          assert.isBelow(lat, 90)
+          assert.isAbove(lat, -90)
+        })
+      })
+
+      it(`should have features of type polygon or MultiPolygon ${fileName}`, () => {
+        const sample = fs.readFileSync(path.resolve(__dirname, fileName), 'UTF-8')
+
+        const parsedFeatureCollection = wfsFeatureCollectionToGeoJSON(sample)
+
+        geometriesOf(parsedFeatureCollection).forEach(geometry => {
+          assert.include(['Polygon', 'MultiPolygon'], geometry.type)
+        })
+      })
+    })
+  })
+
   describe(`Sample ${workingSample3FeatFileName}`, () => {
     const sampleFileName = path.resolve(__dirname, workingSample3FeatFileName)
     let sample
@@ -62,14 +108,99 @@ describe('wfsToGeoJSON function', () => {
       parsedFeatureCollection.features.forEach(feature => assert.ok(isFeature(feature)))
     })
 
-    it('should have features with only EPSG:4326 coordinates', () => {
+    afterEach(() => {
+      sample = null
+    })
+  })
+
+  describe(`Sample ${workingSampleCwFileName}`, () => {
+    const sampleFileName = path.resolve(__dirname, workingSampleCwFileName)
+    let sample
+
+    beforeEach(() => {
+      return fs.readFile(sampleFileName, 'UTF-8')
+        .then(sampleContent => { sample = sampleContent })
+    })
+
+    it('should have 1 feature', () => {
       const parsedFeatureCollection = wfsFeatureCollectionToGeoJSON(sample)
 
-      positionsOf(parsedFeatureCollection).forEach(([lng, lat]) => {
-        assert.isBelow(lng, 180)
-        assert.isAbove(lng, -180)
-        assert.isBelow(lat, 90)
-        assert.isAbove(lat, -90)
+      assert.nestedPropertyVal(parsedFeatureCollection, 'features.length', 1)
+      parsedFeatureCollection.features.forEach(feature => assert.ok(isFeature(feature)))
+    })
+
+    it('should return clowises Features', () => {
+      const parsedFeatureCollection = wfsFeatureCollectionToGeoJSON(sample)
+
+      geometriesOf(parsedFeatureCollection).forEach(geometry => {
+        const validateRings = rings => {
+          const [
+            outerRing,
+            ...innerRings
+          ] = geometry.coordinates
+          assert.isOk(isClockWise(outerRing))
+          innerRings.forEach(innerRing => assert.isNotOk(isClockWise(innerRings)))
+        }
+
+        switch (geometry.type) {
+          case 'Polygon':
+            validateRings(geometry.coordinates)
+            break
+          case 'MultiPolygon':
+            geometry.coordinates.forEach(polygonRings => validateRings(polygonRings))
+            break
+          default:
+            assert.fail('Geometry should be Polygon or MultiPolygon')
+            break
+        }
+      })
+    })
+
+    afterEach(() => {
+      sample = null
+    })
+  })
+
+  describe(`Sample ${workingSampleCcwFileName}`, () => {
+    const sampleFileName = path.resolve(__dirname, workingSampleCcwFileName)
+    let sample
+
+    beforeEach(() => {
+      return fs.readFile(sampleFileName, 'UTF-8')
+        .then(sampleContent => { sample = sampleContent })
+    })
+
+    it('should have 1 feature', () => {
+      const parsedFeatureCollection = wfsFeatureCollectionToGeoJSON(sample)
+
+      assert.nestedPropertyVal(parsedFeatureCollection, 'features.length', 1)
+      parsedFeatureCollection.features.forEach(feature => assert.ok(isFeature(feature)))
+    })
+
+    it('should return clowises Features', () => {
+      const parsedFeatureCollection = wfsFeatureCollectionToGeoJSON(sample)
+
+      geometriesOf(parsedFeatureCollection).forEach(geometry => {
+        const validateRings = rings => {
+          const [
+            outerRing,
+            ...innerRings
+          ] = geometry.coordinates
+          assert.isOk(isClockWise(outerRing))
+          innerRings.forEach(innerRing => assert.isNotOk(isClockWise(innerRings)))
+        }
+
+        switch (geometry.type) {
+          case 'Polygon':
+            validateRings(geometry.coordinates)
+            break
+          case 'MultiPolygon':
+            geometry.coordinates.forEach(polygonRings => validateRings(polygonRings))
+            break
+          default:
+            assert.fail('Geometry should be Polygon or MultiPolygon')
+            break
+        }
       })
     })
 
@@ -79,7 +210,7 @@ describe('wfsToGeoJSON function', () => {
   })
 
   describe(`Sample ${notWorkingSampleEmptyFileName}`, () => {
-    it('should have 0 features', () => {
+    it('should have 0 feature', () => {
       const sample = fs.readFileSync(path.resolve(__dirname, notWorkingSampleEmptyFileName), 'UTF-8')
       const parsedFeatureCollection = wfsFeatureCollectionToGeoJSON(sample)
 
